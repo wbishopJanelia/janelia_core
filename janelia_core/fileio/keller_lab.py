62e81c98f67750de51a882168fa87182492fa1c1
William Bishop <bishopw@hhmi.org>
2018-11-02 01:31:59 -0400

    Adding latest from Windows box after adding code to move Keller data around and testing out speed of loading data on Windows box. Everything seems to be working.

--- janelia_core/fileio/keller_lab.py
+++ janelia_core/fileio/keller_lab.py
@@ -6,27 +6,33 @@
 
 import glob
 import numpy as np
+import os
 import pathlib
+from shutil import copyfile
 from xml.etree import ElementTree as ET
 
 import janelia_core.dataprocessing.dataset
 import janelia_core.dataprocessing.dataset as dataset
 import janelia_core.fileio.exp_reader as exp_reader
 
+# Indices in image file names indicating which sample they correspond to
+IMAGE_NAME_SMP_START_IND = 8
+IMAGE_NAME_SMP_END_IND = 14
 
-def read_exp(image_folder: pathlib.Path, meta_data_folder: pathlib.Path,
-             image_ext: str='.weightFused.TimeRegistration.klb', meta_data_file: str='ch0.xml',
+
+def read_exp(image_folder: pathlib.Path, metadata_folder: pathlib.Path,
+             image_ext: str='.weightFused.TimeRegistration.klb', metadata_file: str= 'ch0.xml',
              time_stamps_file:str = 'TM elapsed times.txt', verbose=True) -> janelia_core.dataprocessing.dataset.DataSet:
     """Function to read in data from a Keller lab VNC experiment.
 
-    This function assumes all the imaging files are stored the one img_folder in the form:
-        img_folder/TM[smp_number]/*.image_ext
+    This function assumes all the imaging files are stored the one image_folder in the form:
+        image_folder/TM[smp_number]/*.image_ext
 
     It also assumes the file with the metadata is stored at:
-        meta_data_folder/meta_data_file
+        metadata_folder/meta_data_file
 
     and the time point data is stored at:
-        meta_data_folder/time_stamps_file
+        metadata_folder/time_stamps_file
 
     This function will read all experimental data into a DataSet object.  Imaging data will be represented as a
     dask.array object.
@@ -34,11 +40,11 @@ def read_exp(image_folder: pathlib.Path, meta_data_folder: pathlib.Path,
     Args:
         image_folder: The folder containing the images (see above) as a pathlib.Path object
 
-        meta_data_folder: The folder containing metadata (see above) as a pathlib.Path object
+        metadata_folder: The folder containing metadata (see above) as a pathlib.Path object
 
         image_ext: String to use when searching for files with the extension for image files.
 
-        meta_data_file: Name of file containing metadata.
+        metadata_file: Name of file containing metadata.
 
         time_stamps_file: Name of file containing time points each image volume was acquired at
 
@@ -55,8 +61,8 @@ def read_exp(image_folder: pathlib.Path, meta_data_folder: pathlib.Path,
 
     """
 
-    metadata = read_metadata(meta_data_folder / meta_data_file)
-    time_stamps = read_time_stamps(meta_data_folder / time_stamps_file)
+    metadata = read_metadata(metadata_folder / metadata_file)
+    time_stamps = read_time_stamps(metadata_folder / time_stamps_file)
     image_names, dask_array = read_images(image_folder, image_ext, verbose)
 
     n_images = dask_array.shape[0]
@@ -171,11 +177,6 @@ def read_images(image_folder: pathlib.Path, image_ext: str='.weightFused.TimeReg
     Raises:
         RuntimeError: If no image files are located.
     """
-
-    # Indices in file names indicating which sample they correspond to
-    start_smp_n_ind = 8
-    end_smp_n_ind = 14
-
     if verbose:
         print('Searching for image files...')
     img_files = glob.glob(str(image_folder / '*' / ('*' + image_ext)))
@@ -186,7 +187,7 @@ def read_images(image_folder: pathlib.Path, image_ext: str='.weightFused.TimeReg
 
     # Make sure our image files are sorted
     files_as_paths = [pathlib.Path(f) for f in img_files]
-    smp_inds = np.asarray([int(f.name[start_smp_n_ind:end_smp_n_ind]) for f in files_as_paths])
+    smp_inds = np.asarray([int(f.name[IMAGE_NAME_SMP_START_IND:IMAGE_NAME_SMP_END_IND]) for f in files_as_paths])
     sort_order = np.argsort(smp_inds)
     sorted_files_as_paths = [files_as_paths[sort_order[i]] for i in sort_order]
 
@@ -197,3 +198,88 @@ def read_images(image_folder: pathlib.Path, image_ext: str='.weightFused.TimeReg
     if verbose:
         print('Done reading in image files.')
     return [sorted_files_as_paths, dask_array]
+
+
+def copy_exp(image_folder: pathlib.Path, metadata_folder: pathlib.Path, dest_folder: pathlib.Path,
+             image_ext: str='.weightFused.TimeRegistration.klb', metadata_file: str='ch0.xml',
+             time_stamps_file:str = 'TM elapsed times.txt', verbose=True, update_int=10):
+    """Function to copy data for a Keller lab VNC experiment from one set of locations to another.
+
+        This function can be useful for copying files from a network drive to location storage.
+
+        Images, metadata and timestamp data are copied to the same destination folder (even
+        though they may be in different source folders).
+
+        This function assumes all the source imaging files are stored the one image_folder in the form:
+            image_folder/TM[smp_number]/*.image_ext
+
+        It also assumes the file with the metadata is stored at:
+            metadata_folder/meta_data_file
+
+        and the time point data is stored at:
+            metadata_folder/time_stamps_file
+
+        Args:
+            image_folder: The folder containing the images (see above) as a pathlib.Path object
+
+            metadata_folder: The folder containing metadata (see above) as a pathlib.Path object
+
+            dest_folder: The destination folder to copy to as a pathlib.Path object
+
+            image_ext: String to use when searching for files with the extension for image files.
+
+            metadata_file: Name of file containing metadata.
+
+            time_stamps_file: Name of file containing time points each image volume was acquired at
+
+            verbose: True if progress updates should be printed to screen
+
+            update_int: How many files should be copied before updating user on status
+
+        Raises:
+            RuntimeError: If destination metadata or images folders already exist
+    """
+
+    # Copy metadata
+    if verbose:
+        print('Copying metadata and time stamp files...')
+    new_metadata_folder = pathlib.Path(dest_folder / 'metadata')
+    if os.path.isdir(new_metadata_folder):
+        raise(RuntimeError('The directory ' + str(new_metadata_folder) + ' already exists.'))
+    os.mkdir(new_metadata_folder)
+    copyfile(metadata_folder / metadata_file, new_metadata_folder / metadata_file)
+    copyfile(metadata_folder / time_stamps_file, new_metadata_folder / time_stamps_file)
+    if verbose:
+        print('Done.')
+
+    # Copy images
+    if verbose:
+        print('Searching for image files...')
+    img_files = glob.glob(str(image_folder / '*' / ('*' + image_ext)))
+    n_img_files = len(img_files)
+
+    if n_img_files == 0:
+        raise(RuntimeError('Unable to find any ' + image_ext + ' files in the subfolders of ' + str(image_folder)))
+    if verbose:
+        print('Done.')
+        print('Copying image files...')
+
+    new_images_folder = pathlib.Path(dest_folder / 'images')
+    if os.path.isdir(new_images_folder):
+        raise (RuntimeError('The directory ' + str(new_images_folder) + ' already exists.'))
+    os.mkdir(new_images_folder)
+
+    for i, f in enumerate(img_files):
+        source_file = pathlib.Path(f)
+        containing_folder = source_file.parts[-2]
+        new_containing_folder = new_images_folder / containing_folder
+        os.mkdir(new_containing_folder)
+        dest_file =  new_containing_folder / source_file.name
+        copyfile(source_file, dest_file)
+        if verbose and (i + 1) % update_int == 0:
+            print('Done copying file ' + str(i + 1) + ' of ' + str(n_img_files) + '.')
+
+    if verbose:
+        print('Done.')
+        print('All files copied successfully.')
+
