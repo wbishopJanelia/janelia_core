6f36c4e8d67209a8c38c272958bb7aac7f79162c
William Bishop <willbishop@gmail.com>
2018-11-26 18:08:41 -0500

    Added code for viewing ROIs.

--- janelia_core/visualization/roi_exploration.py
+++ janelia_core/visualization/roi_exploration.py
@@ -5,6 +5,7 @@
 """
 
 import numpy as np
+import PyQt5
 from PyQt5.QtCore import Qt
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QWidget
@@ -15,6 +16,7 @@ import pyqtgraph as pg
 from pyqtgraph import InfiniteLine
 from pyqtgraph import LinearRegionItem
 
+from janelia_core.math.basic_functions import u_th
 
 roi_clrs = np.asarray([[0, 153, 255], # Blue
                       [51, 204, 51], # Green
@@ -108,17 +110,51 @@ class TimeLine(QWidget):
 
 
 class ROIViewer(QWidget):
+    """ An object for viewing ROI information across time within a single plane.
 
-    def __init__(self, mn_img, rois):
+    Rois are displayed on top of a background image. The window will have a slider that allows the user to scroll
+    through time.  The window also responds to left and right arrow input to scroll through time.
+    """
+    sigKeyPress = pyqtSignal(object)
+
+    def __init__(self, bg_img, rois, roi_vl_str='vls', clrs=None):
+        """ Creates an ROIViewer object.
+
+        Args:
+            bg_img: The background image.  Should be a 2-d numpy array (first dimension y, second dimension x).
+
+            rois: A list of rois.  Each entry is a dictionary with the fields:
+                x - a 1 dimensional np.ndarray of x coordinates of pixels in the ROI
+                y - a 1 dimensional np.ndarray of y coordinates of pixels in the ROI
+                w - a 1 dimensional np.ndarray of weights of each pixel in the ROI
+                vls - a 1 dimensional np.ndarray of values the ROI takes on across time.  Should be within 0 to 1.
+
+            roi_vl_str - If the values of the ROI across time are stored in a field with a different name than 'vls',
+            the user can specify that with this argument.
+
+            clrs - If not note, a np.ndarray of shape n_rois * 3, where each row specifies the color of an roi.  The dtype
+            clrs should be int. And all entries should be within 0 and 255.
+
+        """
         super().__init__()
 
-        self.mn_img = mn_img
+        self.mn_img = bg_img
         self.rois = rois
+        self.roi_vl_str = roi_vl_str
+        self.slider = QSlider(Qt.Horizontal)
+
+        if clrs is None:
+            n_rois = len(rois)
+            clrs = np.zeros([n_rois, 3], dtype=np.int)
+            for i in range(n_rois):
+                clr_ind = i % roi_clrs.shape[0]
+                clrs[i, :] = roi_clrs[clr_ind, :]
+        self.clrs = clrs
 
     def init_ui(self):
+        """ Initializes and shows the interface for the ROIViewer."""
 
-        n_tm_pts = len(self.rois[0]['vls'])
-
+        n_tm_pts = len(self.rois[0][self.roi_vl_str])
 
         # Create a viewbox for our images - this will allow us to pan and scale things
         image_vew_box = pg.ViewBox()
@@ -131,12 +167,9 @@ class ROIViewer(QWidget):
         roi_image_items = [None]*len(self.rois)
         for i, roi in enumerate(self.rois):
 
-            clr_ind = i%roi_clrs.shape[0]
-            clr = roi_clrs[clr_ind,:]
 
-            roi_image = self._gen_roi_image(i, clr)
-            roi_image_items[i] = pg.ImageItem(roi_image)
-            roi_image_items[i].setOpacity(.2)
+            clr = self.clrs[i,:]
+            roi_image_items[i] = self._gen_roi_image(i, clr)
             image_vew_box.addItem(roi_image_items[i])
 
         # Create a graphics view widget, adding our viewbox
@@ -146,19 +179,17 @@ class ROIViewer(QWidget):
         # Define a helper function to set opacity of rois
         def set_roi_opacity(ind):
             for roi_ind, roi in enumerate(self.rois):
-                curVl = roi['vls'][ind]
+                curVl = roi[self.roi_vl_str][ind]
                 roi_image_items[roi_ind].setOpacity(curVl)
 
-        # Create a slider
         def time_pt_changed(ind):
-            print("Slider moved:" + str(ind))
             set_roi_opacity(ind)
 
-        slider = QSlider(Qt.Horizontal)
+        # Setup the slider
+        slider = self.slider
         slider.valueChanged.connect(time_pt_changed)
         slider.setTracking(False)
         slider.setRange(0, n_tm_pts-1)
-
 
         # Layout everything
         layout = QVBoxLayout()
@@ -166,17 +197,63 @@ class ROIViewer(QWidget):
         layout.addWidget(slider)
         self.setLayout(layout)
 
+        # Set things up to respond to key presses
+        def process_key_press(ev):
+            if ev.key() == Qt.Key_Right:
+                slider.setValue(slider.value() + 1)
+            elif ev.key() == Qt.Key_Left:
+                slider.setValue(slider.value() - 1)
+
+        self.sigKeyPress.connect(process_key_press)
+
         self.show()
 
-    def _gen_roi_image(self, roi_ind, clr):
+    def set_value(self, idx):
+        """ Sets the index of the time point the ROI viewer shows.
+
+        Args:
+            idx - the index to show.
+        """
+        self.slider.setValue(idx)
+
+    def _gen_roi_image(self, roi_ind, clr) -> pg.ImageItem:
+        """ Generates an ROI image that can be overlayed the base image.
+
+        Args:
+            roi_ind - The index of self.rois of the roi to generate the image for.
+
+            clr - The color the roi should be shown in.  Should be a numpy array of type int and length 3.  All values
+            should be between 0 and 255.
+
+        Returns:
+            roi_image_item - the image item for the roi
+        """
+        y_min = np.min(self.rois[roi_ind]['y'])
+        y_max = np.max(self.rois[roi_ind]['y'])
+        x_min = np.min(self.rois[roi_ind]['x'])
+        x_max = np.max(self.rois[roi_ind]['x'])
+
+        y_len = y_max - y_min + 1
+        x_len = x_max - x_min + 1
 
         # Set color of all pixels
-        base_image_shape = self.mn_img.shape
-        base_image = np.zeros([*base_image_shape, 4], dtype=np.int)
+        base_image = np.zeros([y_len, x_len, 4], dtype=np.int)
         base_image[:, :, 0:3] = clr
 
-        # Set alpha levels of roi pixels (non-roi pixels are zero)
-        x_pxls = self.rois[roi_ind]['x']
-        y_pxls = self.rois[roi_ind]['y']
+        # Set alpha levels of pixels in the roi
+        y_pxls = self.rois[roi_ind]['y'] - y_min
+        x_pxls = self.rois[roi_ind]['x'] - x_min
         base_image[y_pxls, x_pxls, 3] = np.ndarray.astype(self.rois[roi_ind]['w']*255, np.int)
-        return base_image
+
+        roi_image_item = pg.ImageItem(base_image, autoDownsample=True)
+
+        img_rect = pg.QtCore.QRect(y_min, x_min, y_len, x_len)
+        roi_image_item.setRect(img_rect)
+
+        roi_image_item.setOpacity(.2)
+        return roi_image_item
+
+    def keyPressEvent(self, ev):
+        """ Overrides keyPressEvent of base class, so that we signal to this object that a key has been pressed."""
+        self.sigKeyPress.emit(ev)
+
