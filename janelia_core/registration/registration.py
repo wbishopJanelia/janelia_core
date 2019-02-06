41850481807ab8aa8ec688628225d528f2b71ba0
Bishop <bishopw@hhmi.org>
2019-02-06 17:15:06 -0500

    Refactored registration code.

--- janelia_core/registration/registration.py
+++ janelia_core/registration/registration.py
@@ -28,33 +28,77 @@ from janelia_core.math.basic_functions import divide_into_nearly_equal_parts
 from janelia_core.fileio.exp_reader import read_img_file
 
 
-def register_dataset_images(dataset: DataSet, img_field: str, ref_inds: np.ndarray, sc: pyspark.SparkContext = None,
+def calc_dataset_ref_image(dataset: DataSet, img_field: str, ref_inds: np.ndarray,
+                           median_filter_shape: Sequence[int] = [1, 5, 5],
+                           sc: pyspark.SparkContext = None) -> np.ndarray:
+    """ Calculates a reference image.
+
+        This function will:
+            1) Median filter each plane in all images.
+            2) Calculate a reference image by taking the mean of the mediate filtered images.
+
+        Args:
+            dataset: The dataset containing the images to register.
+
+            img_field: The entry in dataset.ts_data containing the images.
+
+            ref_inds: The indices of images to use for calculating the reference image.
+
+            median_filter_shape: The shape of the median filter to use.
+
+            sc: A spark context to use to distribute computations.  If none, no spark context will be used.
+
+
+        Returns:
+            ref_image: The reference image.
+
+        """
+    t0 = time()
+
+    def med_filter_image(img):
+        return median_filter(img.astype('float32'), median_filter_shape)
+
+    print('Calculating reference image for registration.')
+    ref_image_files = [dataset.ts_data[img_field]['vls'][i]['file'] for i in ref_inds]
+    ref_stats = std_through_time(ref_image_files, preprocess_func=med_filter_image, sc=sc)
+    t1 = time()
+    print('Done calculating reference image.  Elapsed time: ' + str(t1 - t0))
+
+    return ref_stats['mean']
+
+
+def register_dataset_images(dataset: DataSet, img_field: str, ref_image: np.ndarray,
                             median_filter_shape: Sequence[int] = [1, 5, 5], reg_params: dict() = None,
-                            t_field_name: str = 'shift_transform'):
+                            t_field_name: str = 'shift_transform', reg_inds: np.ndarray = None, over_write:bool =True,
+                            sc: pyspark.SparkContext = None,):
 
     """ Calculates transforms to register images through time.
 
     Currently this function calculates translations in x-y.  This function will:
         1) Median filter each plane in all images.
-        2) Calculate a reference image by taking the mean of a set of user specified median filtered images.
-        3) Calculate the max projection of the reference image.
-        4) Calculate the optimal translation to align the max projection of each median filtered image to the
-           max projection of the reference image.
+        2) Calculate the optimal translation to align the max projection of each median filtered image to the
+           the reference image.
 
     Args:
         dataset: The dataset containing the images to register.
 
         img_field: The entry in dataset.ts_data containing the images.
 
-        ref_inds: The indices of images to use for calculating the reference image.
-
-        sc: A spark context to use to distribute computations.  If none, no spark context will be used.
+        ref_image: The reference image to align to.
 
         median_filter_shape: The shape of the median filter to use.
 
         reg_params: A dictionary of optional parameters to provide to the call to estimate_translation
 
         t_field_name: Tha name to save the transform under with each image entry in the dataset.
+
+        reg_inds: Indices of images to register.  If None, all images will be registered (unless over_write is set
+        to False, in which case only unregistered images will be registered.)
+
+        over_write: If true, all images are registered.  If false, only those that do not yet have a
+        transform entry in their dictionary are registered.
+
+        sc: A spark context to use to distribute computations.  If none, no spark context will be used.
 
     Returns: None. Each entry in the images list (which is a dictionary) will have an the transform
     for that image added as a 'transform' entry of the dictionary.
@@ -69,15 +113,6 @@ def register_dataset_images(dataset: DataSet, img_field: str, ref_inds: np.ndarr
     def med_filter_image(img):
         return median_filter(img.astype('float32'), median_filter_shape)
 
-    # Get the reference image we will register to
-    print('Calculating reference image for registration.')
-    ref_image_files = [dataset.ts_data[img_field]['vls'][i]['file'] for i in ref_inds]
-    ref_stats = std_through_time(ref_image_files, preprocess_func=med_filter_image, sc=sc)
-    ref_image = ref_stats['mean'].max(0)
-
-    t1 = time()
-    print('Done calculating reference image.  Elapsed time: ' + str(t1 - t0))
-
     # Calculate transforms for each image
     def reg_func(img):
         img = get_image_data(img)
@@ -85,22 +120,39 @@ def register_dataset_images(dataset: DataSet, img_field: str, ref_inds: np.ndarr
         img = np.max(img, 0)
         return estimate_translation(ref_image, img, **reg_params)
 
-    all_images = [d['file'] for d in dataset.ts_data[img_field]['vls']]
-    all_images = all_images[0:20]
-    n_images = len(all_images)
+    # Determine which images we still need to register
+    img_dicts = dataset.ts_data[img_field]['vls']
+    n_images = len(img_dicts)
+
+    if reg_inds is None:
+        reg_inds = np.arange(0, n_images, 1)
+
+    # See what images have not been registered
+    if not over_write:
+        unreg_image_inds = np.where([t_field_name not in d for d in img_dicts])[0]
+    else:
+        unreg_image_inds = np.arange(0, n_images)
+
+    # Only register those images that we are marked as not registered and user has specified we are to register
+    unreg_image_inds = np.intersect1d(unreg_image_inds, reg_inds)
+
+    # Register images
+    unreg_images = [img_dicts[i]['file'] for i in unreg_image_inds]
+    n_unreg_images = len(unreg_images)
+
     print('Calculating registration transforms.')
     if sc is None:
-        print('Processing ' + str(n_images) + ' images without spark.')
-        transforms = [reg_func(img) for img in all_images]
+        print('Processing ' + str(n_unreg_images) + ' images without spark.')
+        transforms = [reg_func(img) for img in unreg_images]
     else:
-        print('Processing ' + str(n_images) + ' images with spark.')
-        transforms = sc.parallelize(all_images).map(reg_func).collect()
+        print('Processing ' + str(n_unreg_images) + ' images with spark.')
+        transforms = sc.parallelize(unreg_images).map(reg_func).collect()
     t1 = time()
     print('Done calculating registration transforms.  Elapsed time: ' + str(t1 - t0))
 
     # Put transforms with images
     for t_i, t in enumerate(transforms):
-        dataset.ts_data[img_field]['vls'][t_i][t_field_name] = t
+        img_dicts[unreg_image_inds[t_i]][t_field_name] = t
 
 
 def estimate_translation(fixed: np.ndarray, moving: np.ndarray, metric_sampling: float=1.0,
