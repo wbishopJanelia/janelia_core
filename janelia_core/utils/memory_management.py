4d18ed5f6a4571816340699a255df1c588c3fd1a
William Bishop <bishopw@f12u34.int.janelia.org>
2021-03-16 15:03:53 -0400

    Adding latest changes.

---
+++ janelia_core/utils/memory_management.py
@@ -0,0 +1,17 @@
+""" Contains tools for working with memory. """
+
+import os
+import psutil
+
+
+def get_current_memory_usage():
+    """ Returns the memory used by the process this function is called from.
+
+    Returns:
+        mem: Memory used in gigabytes
+    """
+
+    MB_TO_GB = 1024**3
+
+    process = psutil.Process(os.getpid())
+    return process.memory_info()[0]/MB_TO_GB
